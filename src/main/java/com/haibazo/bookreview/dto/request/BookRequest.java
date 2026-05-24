e1ae2577dbafcc33965547109b0ab1c0b1d7057c
Trung <trungnnde180575@fpt.edu.vn>
2026-05-24 22:47:02 +0700

    fix bug update book

--- src/main/java/com/haibazo/bookreview/dto/request/BookRequest.java
+++ src/main/java/com/haibazo/bookreview/dto/request/BookRequest.java
@@ -3,12 +3,16 @@ package com.haibazo.bookreview.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class BookRequest {
     @NotBlank(message = "Name of book is required")
